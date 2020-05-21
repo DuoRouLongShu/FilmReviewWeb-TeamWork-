70a9abdd6cadbec65191ba0850103fdd0fe99d06
smart6666 <2630080680@qq.com>
2020-05-21 13:22:39 +0800

    改bug

---
+++ src/com/FilmReviewWeb/Dao/UserDao.java
@@ -0,0 +1,81 @@
+package com.FilmReviewWeb.Dao;
+
+import com.FilmReviewWeb.Model.User;
+import com.FilmReviewWeb.Utils.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    /**
+     * 检验用户名是否注册
+     * @param Username
+     * @return
+     * @throws SQLException
+     */
+    public boolean validateUsername(String username) throws SQLException{
+        Connection connection = JDBCUtils.getConnection();
+        String sql = "select count(1) from user where username = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,username);
+        Number number = (Number) preparedStatement.executeQuery();
+        return number.intValue() == 0;
+    }
+    /**
+     * 根据用户名查询用户信息
+     * @param username
+     * @return
+     */
+    public User findByUsername(String username) throws SQLException {
+
+        Connection connection = JDBCUtils.getConnection();
+        String sql = "select * from user where username = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,username);
+        User user = (User) preparedStatement.executeQuery();
+
+        return user;
+    };
+
+    /**
+     * 用户保存
+     * @param user
+     */
+    public boolean save(User user) throws SQLException{
+        boolean hasInsert = true;
+        Connection connection = JDBCUtils.getConnection();
+        String sql = "insert user " +
+                "(user_id,user_name,password,power)" +
+                "values (?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1,user.getUserId());
+        preparedStatement.setString(2,user.getUserName());
+        preparedStatement.setString(3,user.getPassword());
+        preparedStatement.setInt(4,user.getPower());
+        //数据库更新的条数
+        int i = preparedStatement.executeUpdate();
+        if (i == 0){
+            hasInsert = false;
+        }
+        JDBCUtils.close(connection,preparedStatement);
+        return hasInsert;
+    }
+
+    /**
+     * 根据用户名查询密码
+     * @param userName
+     * @param password
+     * @return
+     */
+    public User findByUsernameAndPassword(String userName, String password) throws SQLException {
+        Connection connection = JDBCUtils.getConnection();
+        String sql = "select * from user where username = ? and password = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,userName);
+        User user = (User) preparedStatement.executeQuery();
+
+        return user;
+    }
+}
---
+++ src/com/FilmReviewWeb/Servlet/User/FindUserServlet.java
@@ -0,0 +1,30 @@
+package com.FilmReviewWeb.Servlet.User;
+
+import com.alibaba.fastjson.JSON;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/findUserServlet")
+public class FindUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        //从session中获取登录用户
+        Object user = req.getSession().getAttribute("user");
+        //将user写回客户端
+        resp.getWriter().print(JSON.toJSONString(user));
+
+
+    }
+}
